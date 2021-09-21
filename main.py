07bdcb0b17f03be2a85370fddb13369fbf319cdf
BiBiN BeNnY <bibinmbenny@gmail.com>
2021-09-21 05:51:51 +0000

    Age Validation added

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 name = input('What is your name?\n')
 print('Hi, %s.' % name)
+age = int(input('What is your age?\n'))
+if age > 18 :
+ print('Big man, %s.' % name)
+else :
+ print('Small man, %s.' % name)
